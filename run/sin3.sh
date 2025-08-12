76ff6b83d4690336a34f3f70e173c0c9a52d2167
Dominik Tomaszuk <d.tomaszuk@uwb.edu.pl>
2025-08-12 19:30:24 +0200

    add help msg

--- run/sin3.sh
+++ run/sin3.sh
@@ -6,38 +6,54 @@ else
     DATE_CMD="date"   # Linux or GNU coreutils already default
 fi
 
-usage() { echo "Usage: [-s <sparql>] [-d <data>] [-m <mode>] ([-q <query>]) ([-v <verbose>]) ([-r <result>])" 1>&2; exit 1; }
+usage() {
+    echo "Usage: $0 -s <sparql> -d <data> -m <mode>(bwd|fwd) [-q <query>] [-v] [-r <result>]"
+    echo
+    echo "Parameters:"
+    echo "  -s <sparql>   Path to SPARQL query file"
+    echo "  -d <data>     Path(s) to Turtle data file(s), comma-separated"
+    echo "  -m <mode>     Mode: 'bwd' (backward) or 'fwd' (forward)"
+    echo "  -q <query>    Optional query file for execution in bwd mode"
+    echo "  -v            Enable verbose output"
+    echo "  -r <result>   Path to store results (default: res/results.n3)"
+    exit 1
+}
 
-while getopts s:d:m:q:vr: option
-do 
-    case "${option}"
-        in
-        s)sparql=${OPTARG};;
-        d)data=${OPTARG};;
-        m)mode=${OPTARG};;
-        q)query=${OPTARG};;
-        v)verbose="true";;
-        r)result=${OPTARG};;
-        *)usage;;
+# Show usage if no arguments
+if [[ $# -eq 0 ]]; then
+    usage
+fi
+
+while getopts s:d:m:q:vr: option; do 
+    case "${option}" in
+        s) sparql=${OPTARG} ;;
+        d) data=${OPTARG} ;;
+        m) mode=${OPTARG} ;;
+        q) query=${OPTARG} ;;
+        v) verbose="true" ;;
+        r) result=${OPTARG} ;;
+        *) usage ;;
     esac
 done
-# echo -e "$sparql $data $query $verbose $results"
 
-# mkdir -p res
-# mkdir -p tmp
+if [[ -z $sparql || -z $data || -z $mode ]]; then
+    echo "Error: Missing required parameter(s)."
+    usage
+fi
 
-# - generate SPIN code
+if [[ "$mode" != "bwd" && "$mode" != "fwd" ]]; then
+    echo "Error: Invalid mode '$mode'. Must be 'bwd' or 'fwd'."
+    usage
+fi
 
 spin_file="tmp/query.spin"
 if [[ $verbose == "true" ]]; then
     echo -e "\n> getting spin <"
 fi
-time_gen_spin=$( TIMEFORMAT="%R"; { time { java -jar sparql2spin.jar -sparql $sparql  > $spin_file; } } 2>&1 )
+time_gen_spin=$( TIMEFORMAT="%R"; { time { java -jar sparql2spin.jar -sparql "$sparql" > "$spin_file"; } } 2>&1 )
 if [[ $verbose == "true" ]]; then
     echo -e "(stored at $spin_file)"
 fi
-
-# - generate N3 code
 
 n3_file="tmp/n3query.n3"
 n3_stderr="tmp/n3query-output.txt"
@@ -46,15 +62,13 @@ if [[ $verbose == "true" ]]; then
     echo -e "\n> getting n3 rules <"
     echo -e "$cmd"
 fi
-time_gen_n3=$( TIMEFORMAT="%R"; { time { eval "$cmd" > $n3_file 2>$n3_stderr; } } 2>&1 )
+time_gen_n3=$( TIMEFORMAT="%R"; { time { eval "$cmd" > "$n3_file" 2>"$n3_stderr"; } } 2>&1 )
 if [[ $verbose == "true" ]]; then
     echo -e "(stored at $n3_file)"
 fi
 
-sed -i'' -e 's|rdf:first|rdf:f1rst|g' $n3_file
-sed -i'' -e 's|rdf:rest|rdf:r3st|g' $n3_file
-
-# - execute N3 rules
+sed -i'' -e 's|rdf:first|rdf:f1rst|g' "$n3_file"
+sed -i'' -e 's|rdf:rest|rdf:r3st|g' "$n3_file"
 
 if [[ -z $result ]]; then
     result="res/results.n3"
@@ -64,11 +78,11 @@ if [[ $verbose == "true" ]]; then
 fi
 
 load_cmd=""
-IFS=','; datas=$data;
-for data in $datas; do
-    load_cmd="$load_cmd --turtle $data"
+IFS=',' read -ra datas <<< "$data"
+for d in "${datas[@]}"; do
+    load_cmd="$load_cmd --turtle $d"
 done
-unset IFS;
+unset IFS
 
 start=$($DATE_CMD +%s%N)
 if [[ $mode == "bwd" ]]; then
@@ -79,7 +93,7 @@ fi
 if [[ $verbose == "true" ]]; then
     echo -e "$cmd"
 fi
-error=$( { eval "$cmd" > $result; } 2>&1 )
+error=$( { eval "$cmd" > "$result"; } 2>&1 )
 end=$($DATE_CMD +%s%N)
 time_exec_total=$(bc -l <<< "scale = 2; ($end-$start)/1000000000")
 
